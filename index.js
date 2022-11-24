5264a4002c178825792b24cbde18d20823128c3a
sohagislampk <sohagislampk@gmail.com>
2022-11-24 18:18:37 +0600

    User create api post method

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
 const port = process.env.PORT || 5000;
 
 
@@ -8,6 +10,32 @@ const app = express();
 //middleware
 app.use(cors());
 app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.9qpmxm2.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run() {
+    try {
+
+        const usersCollenction = client.db('tunetools').collection('users');
+
+        app.post('/users', async (req, res) => {
+            const user = req.body
+            const result = await usersCollenction.insertOne(user);
+            res.send(result)
+        });
+
+
+    }
+    finally {
+
+    }
+}
+run().catch(e => console.error(e));
+
 
 app.get('/', (req, res) => {
     res.send('Tune Tools Server Is Running');
